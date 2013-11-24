60341f9c83317bc8dbaaf9b00a7d7b46bb04b4d2
Kevin Beaty <kevin@simplectic.com>
2013-11-23 22:50:31 -0600

    Add JSON Mapper

---
+++ mapper.py
@@ -0,0 +1,67 @@
+class JSONMapper(object):
+    def __init__(self, *attrs, **json_attrs):
+        self.attributes = set(attrs) | set(json_attrs)
+        self.json_attrs = dict((attr, self.json_attr(attr)) for attr in attrs)
+        self.json_attrs.update(json_attrs)
+
+    def json_attr(self, attr):
+        return attr
+
+    def to_json(self, source):
+        return self._map_json(source, None, True)
+
+    def from_json(self, source, destination=None):
+        return self._map_json(source, destination, False)
+
+    def _map_json(self, source, destination, to_json):
+        destination = destination or {}
+
+        attributes = self.attributes
+        for attr in attributes:
+            if self._hasattr(source, attr, to_json):
+                value = self._getattr(source, attr, to_json)
+                self._setattr(destination, attr, value, to_json)
+
+        return destination
+
+    def _hasattr(self, source, attr, to_json):
+        if not to_json:
+            attr = self.json_attrs.get(attr, attr)
+
+        if isinstance(source, dict):
+            return attr in source
+
+        return hasattr(source, attr)
+
+    def _getattr(self, source, attr, to_json):
+        if not to_json:
+            attr = self.json_attrs.get(attr, attr)
+
+        if isinstance(source, dict):
+            return source[attr]
+
+        return getattr(source, attr)
+
+    def _setattr(self, destination, attr, value, to_json):
+        if to_json:
+            attr = self.json_attrs.get(attr, attr)
+
+        if isinstance(destination, dict):
+            destination[attr] = value
+        else:
+            setattr(destination, attr, value)
+
+
+class JSONCamelMapper(JSONMapper):
+    def json_attr(self, attr):
+        cs = []
+        upper = False
+        for c in attr:
+            if c == '_':
+                upper = True
+            elif upper:
+                cs.append(c.upper())
+                upper = False
+            else:
+                cs.append(c)
+        return ''.join(cs)
